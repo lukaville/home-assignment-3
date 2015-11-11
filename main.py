a097bc604a7f1e98a9541b7f75ff9f289d0b0c88
Nickolay <lukaville@gmail.com>
2015-11-11 23:30:47 +0300

    except only ValueError, float parsing

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def main():
     try:
         opeator = str(raw_input("Enter operator name: "))
         arguments_string = str(raw_input("Enter arguments: "))
-        arguments = map(lambda x: int(x), arguments_string.split())
+        arguments = map(lambda x: float(x), arguments_string.split())
         print(calculate(opeator, *arguments))
-    except Exception as e:
+    except ValueError as e:
         print("Sorry, error occurred: " + e.message)
 
 if __name__ == "__main__":
